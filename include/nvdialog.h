f004b109e8f5832f6a7af551eac69dccb5a5bb65
Aggelos Tselios <androtechgr@gmail.com>
2022-09-01 14:59:37 +0300

    nvdialog.h: Make nvd_init() take argv[0] as argument.

--- include/nvdialog.h
+++ include/nvdialog.h
@@ -1,25 +1,25 @@
- /*
+/*
  *  The MIT License (MIT)
  *
  *  Copyright (c) 2022 Aggelos Tselios
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
+ *  of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 #ifndef __nvdialog_h__
@@ -35,7 +35,7 @@
  * @brief The core type for managing and identifying dialog boxes.
  * This struct should be returned from the library and not be shared across
  * multiple threads / uses.
-*/
+ */
 typedef struct _NvdDialogBox NvdDialogBox;
 
 /**
@@ -46,7 +46,7 @@ typedef struct _NvdDialogBox NvdDialogBox;
  * - NVD_DIALOG_WARNING -> A warning message to be used for warnings.
  * - NVD_DIALOG_ERROR -> An error message box.
  * - NVD_DIALOG_QUESTION -> Currently unsupported.
-*/
+ */
 typedef enum {
         NVD_DIALOG_SIMPLE = 0xff,
         NVD_DIALOG_WARNING,
@@ -62,12 +62,12 @@ typedef struct {
         short major;
         short minor;
         short patch;
-        char* string;
+        char *string;
 } NvdVersion;
 
 /**
  * @brief Returns the version of nvdialog currently linked with.
- * For a compile time alternative implementation see the 
+ * For a compile time alternative implementation see the
  * NVDIALOG_VERSION_MAJOR, NVDIALOG_VERSION_MINOR and NVDIALOG_VERSION_PATCH
  * constants.
  */
@@ -77,9 +77,10 @@ NvdVersion nvd_get_version();
  * @brief Initializes the library.
  * This function is only required for compatibility with every platform. You
  * can safely ignore this function if you only target a specific platform.
+ * @param program The argv[0] of your program.
  * @return 0 on success, else a negative error code indicating failure.
  */
-uint32_t nvd_init();
+uint32_t nvd_init(char *program);
 
 /**
  * @brief Creates a new dialog object and returns it.
@@ -87,9 +88,8 @@ uint32_t nvd_init();
  * and returns an opaque handle to it for use with @ref nvd_show_dialog().
  * @return The dialog handle on success, else NULL.
  */
-NvdDialogBox *nvd_dialog_box_new(const char* title,
-                                const char* message,
-                                NvdDialogType type);
+NvdDialogBox *nvd_dialog_box_new(const char *title, const char *message,
+                                 NvdDialogType type);
 /**
  * @brief Shows a dialog to the system shell.
  * @param dialog The dialog to show.
