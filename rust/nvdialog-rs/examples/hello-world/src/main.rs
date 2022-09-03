8e3c643c1350c10ed0c996f8e96bc4c580de7819
Aggelos Tselios <androtechgr@gmail.com>
2022-09-03 15:13:12 +0300

    hello-world/: Implement example.

---
+++ rust/nvdialog-rs/examples/hello-world/src/main.rs
@@ -0,0 +1,17 @@
+extern crate nvdialog;
+use nvdialog::{NvDialogContext, DialogType::*};
+
+fn main() {
+    let mut argv_0: String = String::new();
+    for (i, arg) in std::env::args().enumerate() {
+        if i == 0 { argv_0 = arg } else { break; }
+    };
+    let ctx = NvDialogContext::new().init(&argv_0);
+    ctx.message_box(
+        "Hello World!",
+        "This is a dialog created with the Rust bindings of libnvdialog.\n
+        Newlines and other common features are supported!
+        ",
+        SimpleDialog
+    )
+}
