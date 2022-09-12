074e0bc01089a5931dee17e91445d54d2c22ef16
Aggelos Tselios <androtechgr@gmail.com>
2022-09-12 21:19:33 +0300

    nvdialog_assert.h: Create custom assertion macro.
    Instead of using the standard 'assert(), a new assertion macro has been added specifically for nvdialog. The main advantage is that it does not crash the entire application if the assertion fails, but just print a message.'

---
+++ src/nvdialog_assert.h
@@ -0,0 +1,60 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2022 Aggelos Tselios
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef __nvdialog_assert_h__
+#define __nvdialog_assert_h__
+
+#include "nvdialog.h"
+#include "nvdialog_error.h"
+#include "nvdialog_macros.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+static void __attribute__((format(printf, 1, 2)))
+nvd_print_assert(const char *msg, ...) {
+        va_list args;
+        va_start(args, msg);
+
+        char buffer[NVD_BUFFER_SIZE * 16]; /* I know. Huge buffer. But we could
+                                              be dealing with worse things. */
+        sprintf(buffer, "%s %s", TERMINAL_PREFIX, msg);
+        vfprintf(stderr, buffer, args);
+
+        va_end(args);
+        fflush(stderr);
+}
+
+#define NVD_STRING(s) #s
+
+#define NVD_ASSERT(eq)                                                         \
+        do {                                                                   \
+                if (!(eq)) {                                                    \
+                        nvd_print_assert(                                      \
+                            "Assertion failure: %s\n  Line: %d\n  Filename: "     \
+                            "%s\n  Function: %s\n",                            \
+                            #eq, __LINE__, __FILE__, __func__);                 \
+                }                                                              \
+        } while (0)
+
+#endif /* __nvdialog_assert_h__ */
