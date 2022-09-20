859f03707619d5e4d140d0d9d8ec5cbbe564a95f
Aggelos Tselios <androtechgr@gmail.com>
2022-09-20 21:56:53 +0300

    nvdialog_win32.h: Declare `nvd_about_dialog_win32` prototype.

--- src/backend/nvdialog_win32.h
+++ src/backend/nvdialog_win32.h
@@ -35,3 +35,6 @@ const char *nvd_open_file_dialog_win32(const char *title, const char *message);
  */
 uint32_t nvd_create_win32_dialog(const char *title, const char *message,
                                  NvdDialogType type);
+
+nvd_about_dialog_win32(const char *name, const char *description,
+                       const char *license_text, const char *logo_path);
