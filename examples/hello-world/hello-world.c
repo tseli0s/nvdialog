657cf31945434307e1126e8c8139090a827a79c9
Aggelos Tselios <androtechgr@gmail.com>
2022-10-09 12:37:19 +0300

    hello-world.c: Update example to use the new API.

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -1,5 +1,6 @@
 #include <nvdialog/nvdialog.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
@@ -7,12 +8,21 @@ int main(int argc, char **argv)
         nvd_init(argv[0]);
 
         /* Creating the dialog. */
-        nvd_dialog_box_new(
+        NvdDialogBox* dialog = nvd_dialog_box_new(
                 "Hello World!",
                 "This is a dialog box using \nlibnvdialog. See https://github.com/AndroGR/nvdialog/"
                 "\nfor more details."
                 ,
                 NVD_DIALOG_SIMPLE
         );
+        /* If the dialog couldn't be created, exit. */
+        if (!dialog) return -12;
+
+        /* 
+         * Otherwise show the created dialog. 
+         * Recommendation: You can try running the dialog
+         * to a seperate thread / process to avoid halting the process.
+        */
+        nvd_show_dialog(dialog);
         return argc - 1;
 }
