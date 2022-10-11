2b3888efd263a8c5c481a65f1c20e977dc506877
Aggelos Tselios <androtechgr@gmail.com>
2022-10-11 17:34:20 +0300

    about-dialog.c: Include standard headers.

--- examples/about-dialog/about-dialog.c
+++ examples/about-dialog/about-dialog.c
@@ -1,9 +1,9 @@
-#include "dialogs/nvdialog_about_dialog.h"
-#include "dialogs/nvdialog_dialog_box.h"
 #include <nvdialog/nvdialog.h>
-#include <nvdialog/nvdialog_dialog.h>
 #define APP_NAME "NvDialog Example"
 #define NULL ((void*)0)
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
@@ -16,8 +16,10 @@ int main(int argc, char **argv)
         NvdAboutDialog* dialog = nvd_about_dialog_new(APP_NAME,
                              description, "This program is licensed under the MIT license,"
                              "see https://mitlicense.example for more.", NULL);
-        if (!dialog) return -1;
-        else {
+        if (!dialog) {
+                puts("Couldn't create the dialog object. Aborting.");
+                abort();
+        } else {
                 nvd_show_about_dialog(dialog);
                 return 0;
         }
