ce17a7c211cafc85a2819643648266d92ba65199
Amrit Bhogal <ambhogal01@gmail.com>
2022-12-12 13:35:06 -0800

    Finsih about dialog

--- src/backend/cocoa/nvdialog_about_dialog.m
+++ src/backend/cocoa/nvdialog_about_dialog.m
@@ -2,10 +2,10 @@
 #include "nvdialog_cocoa.h"
 
 #include <AppKit/AppKit.h>
+#include <Foundation/Foundation.h>
 
 struct _NvdAboutDialog {
-    const char *name, *description, *copyright_url;
-    NSImage *image;
+    NSMutableDictionary<NSAboutPanelOptionKey, id> *options;
 };
 
 NvdAboutDialog *nvd_about_dialog_cocoa(const char *name,
@@ -13,14 +13,31 @@ NvdAboutDialog *nvd_about_dialog_cocoa(const char *name,
                                        const char *imgpath)
 {
     NvdAboutDialog *dlg = malloc(sizeof(struct _NvdAboutDialog));
-    dlg->name = name;
-    dlg->description = desc;
-    dlg->copyright_url = NULL;
-    dlg->image = [NSImage imageNamed: @(imgpath)];
+
+    dlg->options = [NSMutableDictionary dictionary];
+    dlg->options[NSAboutPanelOptionApplicationName] = @(name);
+    dlg->options[NSAboutPanelOptionVersion] = @(desc);
+
+    if (imgpath != nil)
+        dlg->options[NSAboutPanelOptionApplicationIcon] = [[NSImage alloc] initWithContentsOfFile: @(imgpath)];
+
     return dlg;
 }
 
 void nvd_about_dialog_set_licence_link_cocoa(NvdAboutDialog *dialog, const char *license_link, const char *txt)
 {
-    
+    (void)txt;
+
+    dialog->options[NSAboutPanelOptionCredits] = @(license_link);
+}
+
+void *nvd_about_dialog_get_raw_cocoa(NvdAboutDialog *dlg)
+{
+    return dlg;
+}
+
+void nvd_show_about_dialog_cocoa(NvdAboutDialog *dialog)
+{
+    [NSApp orderFrontStandardAboutPanelWithOptions: dialog->options];
+    [dialog->options release];
 }
