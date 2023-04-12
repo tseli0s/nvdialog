41dd0038806394941567d258f9bea94fc6d3b185
Aggelos Tselios <aggelostselios777@gmail.com>
2023-04-12 17:40:29 +0300

    sandbox/: Add sandbox backend based on Gtk3.

---
+++ src/backend/sandbox/nvdialog_css_manager.c
@@ -0,0 +1,109 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2023 Aggelos Tselios
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "gtk/gtkcssprovider.h"
+#include "nvdialog.h"
+#include "nvdialog_sbx.h"
+#include "../../nvdialog_assert.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+static char *__nvd_string_gen(char *str, size_t size) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJ";
+    if (size) {
+        --size;
+        for (size_t n = 0; n < size; n++) {
+            int key = rand() % (int)(sizeof charset - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+    return str;
+}
+
+NvdCSSManager *nvd_css_manager_sbx() {
+        /* Only using calloc() here because I don't want to assign NULL everywhere :P */
+        NvdCSSManager *mgr = calloc(1, sizeof(struct _NvdCSSManager));
+        NVD_RETURN_IF_NULL(mgr);
+
+        mgr->raw  = gtk_css_provider_new();
+        mgr->used = false;
+
+        return mgr;
+}
+
+int nvd_css_manager_attach_string_stylesheet_sbx(NvdCSSManager *mgr,
+                                                 const char    *str) {
+        int chdir_result = chdir("/tmp");
+        if (chdir_result != 0) {
+                NVD_ASSERT(chdir_result != 0);
+                return errno;
+        }
+
+        char *new_filename = calloc(1, sizeof(char) * 13);
+        __nvd_string_gen(new_filename, 12);
+
+        FILE *stream = fopen(new_filename, "w");
+        fputs (str, stream);
+        fclose(stream);
+
+        char filename[NVDIALOG_MAXBUF];
+        sprintf(filename, "%s%s", "/tmp/", new_filename);
+
+        bool result = gtk_css_provider_load_from_path ((GtkCssProvider*) mgr->raw,
+                                                      filename,
+                                                      NULL);
+        if (!result) {
+                nvd_set_error(NVD_INTERNAL_ERROR);
+                free(new_filename);
+                return -1;
+        }
+        /*
+         * Well, we could store it in 'mgr' but then why even create a random
+         * string for its filename?
+         */
+        free(new_filename);
+        return 0;
+}
+
+int nvd_css_manager_attach_stylesheet_sbx(NvdCSSManager *mgr, const char *filename) {
+        bool result = gtk_css_provider_load_from_path(mgr->raw,
+                                                      filename,
+                                                      NULL);
+        if (!result) {
+                nvd_set_error(NVD_INTERNAL_ERROR);
+                return -1;
+        }
+        return 0;
+}
+
+NVD_FORCE_INLINE int nvd_css_manager_use_style_sbx(NvdCSSManager *mgr, void *raw_handle) {
+        gtk_style_context_add_provider(
+                gtk_widget_get_style_context(GTK_WIDGET(raw_handle)),
+                mgr->raw,
+                GTK_STYLE_PROVIDER_PRIORITY_USER
+        );
+        return 0;
+}
