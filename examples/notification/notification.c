7ace34aa84950b7b4e6bf6dbe8abb2165efdf722
Aggelos Tselios <aggelostselios777@gmail.com>
2023-04-13 22:18:46 +0300

    notification: Improve example.

--- examples/notification/notification.c
+++ examples/notification/notification.c
@@ -1,6 +1,7 @@
 #include <nvdialog/nvdialog.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
     if (nvd_init(argv[0]) != 0) {
@@ -13,10 +14,14 @@ int main(int argc, char** argv) {
         "Set your notification text here.",
         NVD_NOTIFICATION_SIMPLE
     );
+    if (!notif) {
+        printf("Failed to create notification: %s", nvd_stringify_error(nvd_get_error()));
+        exit(EXIT_FAILURE);
+    }
 
     int pressed_button = 0;
     nvd_add_notification_action(notif, "I like NvDialog :)", 1, &pressed_button);
     nvd_send_notification(notif);
-    
+
     return 0;
 }
