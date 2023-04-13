84d7770e77a00d1a99631efa697947add3d51e42
Aggelos Tselios <aggelostselios777@gmail.com>
2023-04-13 21:13:10 +0300

    file_dialog: Improve example.

--- examples/file-dialog/file_dialog.c
+++ examples/file-dialog/file_dialog.c
@@ -16,20 +16,28 @@ int main(int argc, char** argv) {
                 exit(EXIT_FAILURE);
         }
 
+        /* The file extensions we can use with the dialog. */
+        const char* filters = "img;png;jpeg;ico;svg";
+        
         /* Constructing the dialog. This is the most important part. */
         NvdFileDialog* dialog = nvd_open_file_dialog_new("Open File",
-                                                         NULL);
+                                                         filters);
         if (!dialog) {
                 puts("Error: Could not construct the dialog.");
                 return -ENOMEM;
         }
 
         /* Showing the dialog to the user, then saving the filename to the second parameter. */
-        char* filename;
+        const char* filename;
+        /* 
+         * This function either returns the filename or NULL to where the second parameter points.
+         * As you can see below, NULL means no file was selected (Although it could be an error to,
+         * in which case NvDialog handled it)
+         */
         nvd_get_file_location(dialog, &filename);
-        
-        /* Then, take the file path and print it to stdout. */
-        puts(filename);
+
+        if (filename != NULL) printf("Chosen file: %s\n", filename);
+        else printf("No file selected.\n");
 
         /* Then finally, freeing the dialog. */
         nvd_free_object(dialog);
