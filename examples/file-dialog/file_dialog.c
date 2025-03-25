1fd78afe922cf9f537f12c7f12bc6e07d49633e5
Aggelos Tselios <aggelostselios777@gmail.com>
2025-03-25 18:29:29 +0200

    file_dialog: Add extra code for new API example

--- examples/file-dialog/file_dialog.c
+++ examples/file-dialog/file_dialog.c
@@ -22,25 +22,33 @@ int main(int argc, char** argv) {
         /* Constructing the dialog. This is the most important part. */
         NvdFileDialog* dialog = nvd_open_file_dialog_new("Open File",
                                                          filters);
-        if (!dialog) {
+        /* We will also display a dialog for selecting a folder instead of a dialog. */
+        NvdFileDialog *dir_dialog = nvd_open_folder_dialog_new("Open folder...", NULL);
+
+        if (!dialog || !dir_dialog) {
                 puts("Error: Could not construct the dialog.");
                 return -ENOMEM;
         }
 
         /* Showing the dialog to the user, then saving the filename to the second parameter. */
-        const char* filename;
+        const char *filename, *directory;
         /* 
          * This function either returns the filename or NULL to where the second parameter points.
          * As you can see below, NULL means no file was selected (Although it could be an error to,
          * in which case NvDialog handled it)
          */
         nvd_get_file_location(dialog, &filename);
+        nvd_get_file_location(dir_dialog, &directory);
 
         if (filename != NULL) printf("Chosen file: %s\n", filename);
         else printf("No file selected.\n");
 
+        if (directory != NULL) printf("Chosen directory: %s\n", directory);
+        else printf("No directory selected.\n");
+
         /* Then finally, freeing the dialog. */
         nvd_free_object(dialog);
+        nvd_free_object(dir_dialog);
 
         return 0;
 }
