36adfd46227cff5a9a4fbe4977236bef16e9cf15
Aggelos Tselios <aggelostselios777@gmail.com>
2025-11-14 16:35:29 +0200

    src/image: Only use function bodies if `_NVD_SUPPORT_IMAGES` is defined

--- src/nvdialog_image.c
+++ src/nvdialog_image.c
@@ -24,19 +24,23 @@
 
 #include "nvdialog_assert.h"
 #include "nvdialog_util.h"
+
+#ifdef _NVD_SUPPORT_IMAGES
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ASSERT(x) NVD_ASSERT_FATAL(x)
+#include <stb_image.h>
+#endif /* _NVD_SUPPORT_IMAGES */
+
 #include <nvdialog.h>
 #include <nvdialog_image.h>
-#include <stb_image.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "impl/nvdialog_typeimpl.h"
 
 const uint8_t *nvd_image_from_filename(const char *filename, int *width,
                                        int *height) {
+#ifdef _NVD_SUPPORT_IMAGES
         NVD_ASSERT(filename != NULL);
         FILE *f = fopen(filename, "rb");
         NVD_RETURN_IF_NULL(f);
@@ -51,9 +55,14 @@ const uint8_t *nvd_image_from_filename(const char *filename, int *width,
 
         fclose(f);
         return data;
+#else
+        nvd_set_error(NVD_UNSUPPORTED_ACTION);
+        return NULL;
+#endif
 }
 
 NvdImage *nvd_create_image(const uint8_t *data, int width, int height) {
+#ifdef _NVD_SUPPORT_IMAGES
         NvdImage *image = nvd_calloc(1, sizeof(struct _NvdImage));
         NVD_ASSERT(image != NULL);
         NVD_ASSERT(data != NULL);
@@ -64,16 +73,30 @@ NvdImage *nvd_create_image(const uint8_t *data, int width, int height) {
         image->height = height;
 
         return image;
+#else
+        nvd_set_error(NVD_UNSUPPORTED_ACTION);
+        return NULL;
+#endif
 }
 
 void nvd_destroy_image(NvdImage *image) {
+#ifdef _NVD_SUPPORT_IMAGES
         if (image != NULL) {
                 free((void *)image->data);
                 free(image);
         }
+#else
+        nvd_set_error(NVD_UNSUPPORTED_ACTION);
+        return NULL;
+#endif
 }
 
 uint8_t *nvd_image_to_bytes(NvdImage *image) {
+#ifdef _NVD_SUPPORT_IMAGES
         NVD_ASSERT(image != NULL);
         return image->data;
+#else
+        nvd_set_error(NVD_UNSUPPORTED_ACTION);
+        return NULL;
+#endif
 }
