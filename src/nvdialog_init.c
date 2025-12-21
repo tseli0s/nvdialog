565daaa032040b54183a9cce2e14ba77fd037e4b
Aggelos Tselios <aggelostselios777@gmail.com>
2025-12-21 14:44:37 +0200

    src/init: Warn if nvd_init_backends can't recognize the target
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- src/nvdialog_init.c
+++ src/nvdialog_init.c
@@ -168,7 +168,7 @@ int nvd_init_backends(NvdBackendMask *mask) {
         return nvd_init_win32(mask);
 #elif defined(NVD_COCOA_IMPL)
         return nvd_init_cocoa(mask);
+#else
+#warning Unknown target. NvDialog currently only supports Windows, macOS and Gtk-based desktops.
 #endif
-
-        return -1;
 }
