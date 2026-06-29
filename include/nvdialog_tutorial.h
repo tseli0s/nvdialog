5bc87bca574c63cdb9a71adcd13de8e79e559b1f
Aggelos Tselios <aggelostselios777@gmail.com>
2026-06-29 14:45:22 +0300

    include/tutorial: Drop license header and `__nvd_tutorial` define
    See 1b03fe0 as well. The macro isn't used anywhere, so no reason to have
    it. The header is not part of the source code and therefore doesn't need
    to have a header.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- include/nvdialog_tutorial.h
+++ include/nvdialog_tutorial.h
@@ -1,35 +1,9 @@
-/*
- *  The MIT License (MIT)
- *
- *  Copyright (c) 2025 Aggelos Tselios
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
-
 /************************************************************************************
  * This file is NOT source code. It will just be used by Doxygen when
  * generating documentation for a tutorial page. It won't be installed, it won't
  * be used within the library or by a user, and probably isn't of interest to
  * anyone outside this.
  ************************************************************************************/
-
-#define __nvd_tutorial
 
 /**
  * @page NvDialog Tutorial
